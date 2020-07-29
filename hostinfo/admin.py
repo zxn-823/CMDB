4b6fa2474bbdf7c288837784f5eb1f38c6214b2d
zxn-823 <893789337@qq.com>
2020-07-29 22:57:24 +0800

    第一次上传：数据库设计与后台管理

---
+++ hostinfo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Server
+
+# Register your models here.
+admin.site.register(Server)
+from django.contrib import admin
+
+# Register your models here.
